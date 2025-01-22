45a9d2c2bea694bef46fe382b5e7b57ccfa94880
Sammy-art-creator <svmoralesz@gmail.com>
2025-01-22 16:09:52 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".net", ".us", ".ie"];
+
+  for (let a of pronoun) {
+    for (let b of adj) {
+      for (let c of noun) {
+        for (let d of dom) {
+          console.log(a + b + c + d);
+        }
+      }
+    }
+  }
 };
